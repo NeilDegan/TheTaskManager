b2db170b530610ae37fae9db09be6fb461e790e6
Lukas <Lukas@Lukas-Laptop>
2018-11-08 14:30:49 +0100

    ö

--- TaskManager/src/Aufgabe.java
+++ TaskManager/src/Aufgabe.java
@@ -1,4 +1,26 @@
+import java.util.*;
+import java.text.*;
+import java.sql.*;
 
 public class Aufgabe {
+
+    private String aufgabentext;
+    private GregorianCalender dautumErstellung;
+
+    public GregorianCalender getDautumErstellung() {
+        return dautumErstellung;
+    }
+
+    public void setDautumErstellung(GregorianCalender dautumErstellung) {
+        this.dautumErstellung = dautumErstellung;
+    }
+
+    public String getAufgabentext() {
+        return aufgabentext;
+    }
+
+    public void setAufgabentext(String aufgabentext) {
+        this.aufgabentext = aufgabentext;
+    }
 
 }
